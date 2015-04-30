37c9cb732b4bc21103a0f9365e53a65e8b56a2a6
carlosv5 <carlos.vegagarcia@hotmail.com>
2015-04-30 21:03:25 +0200

    Rama creditos

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/author', function(req,res){
+	res.render('author', {title:'Quiz'});
+});
+
 module.exports = router;
