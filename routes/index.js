c49c6cc3d150a289bc0669fc2dec67d4ad069719
carlosv5 <carlos.vegagarcia@hotmail.com>
2015-05-03 20:47:12 +0200

    Autenticación y sesión

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ var router = express.Router();
 
 var quizController = require('../controllers/quiz_controller');
 var commentController = require('../controllers/comment_controller');
+var sessionController = require('../controllers/session_controller');
 
 /* GET home page. */
 router.get('/', function(req, res) {
@@ -19,6 +20,10 @@ router.post('/quizes/create', quizController.create);
 router.get('/quizes/:quizId(\\d+)/edit', quizController.edit);
 router.put('/quizes/:quizId(\\d+)', quizController.update);
 router.delete('/quizes/:quizId(\\d+)', quizController.destroy);
+//Definicion de rutas de sesion
+router.get('/login',sessionController.new); //formulario login
+router.post('/login', sessionController.create); //crear sesion
+router.get('/logout', sessionController.destroy);//destruir sesion
 router.get('/author', function(req,res){
 	res.render('author', {title:'Quiz' , errors:[]});
 }); //Le he puesto aqui tambien errors
